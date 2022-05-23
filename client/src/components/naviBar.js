189c079dfbfe40d99fb8d0e22cb29d7f22df9f1c
Tyler Cako <tylercakotc@gmail.com>
2022-05-23 11:45:43 -0600

    commit

--- client/src/components/naviBar.js
+++ client/src/components/naviBar.js
@@ -8,13 +8,12 @@ const NaviBar = () => {
     return(
         <nav>
             <a href="/" className="logo">Logo</a>
-            <div className="dropdown">
-                <button className="drop-btn" />
-                <div className="dropdown-menu">
-                    <a href="/">Home</a>
-                    <a href="/">About</a>
-                    <a href="/">Projects</a>
-                </div>
+            <button className="drop-btn" />
+            <div className="dropdown-menu">
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Projects</a>
+                <a className="drop-exit">X</a>
             </div>
         </nav>
     )

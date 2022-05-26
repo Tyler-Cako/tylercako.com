50895293537354483e606d8f0158fe2650cf177f
Tyler Cako <tylercakotc@gmail.com>
2022-05-25 22:12:36 -0600

    commit

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import '../CSS/About.css'
 
 const About = () => {
     return(
         <>
             <div className="about">
-                <img alt="photo" height="500px" width="500px"></img>
+                <img alt="picture" className="tyler-photo"/>
                 <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus in hendrerit gravida rutrum quisque non. Faucibus in ornare quam viverra orci sagittis eu volutpat. Blandit libero volutpat sed cras ornare arcu dui vivamus. Phasellus egestas tellus rutrum tellus pellentesque. Nulla facilisi cras fermentum odio eu. Erat imperdiet sed euismod nisi porta lorem mollis. Quam lacus suspendisse faucibus interdum posuere lorem ipsum. Adipiscing vitae proin sagittis nisl. Id faucibus nisl tincidunt eget nullam non nisi est sit. Nulla malesuada pellentesque elit eget gravida cum sociis natoque. Adipiscing elit ut aliquam purus sit amet luctus. Netus et malesuada fames ac. Tristique senectus et netus et malesuada fames. In fermentum et sollicitudin ac orci phasellus. Etiam sit amet nisl purus in mollis nunc sed. Fringilla est ullamcorper eget nulla facilisi etiam. Sed faucibus turpis in eu mi bibendum neque egestas. Nibh cras pulvinar mattis nunc sed blandit libero volutpat. Lobortis mattis aliquam faucibus purus in massa tempor.
                 </p>

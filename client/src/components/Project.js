9d9ede1c414baf11de67007d02f6f4746f8dd968
Tyler Cako <tylercakotc@gmail.com>
2022-05-28 12:21:44 -0600

    commit

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -13,9 +13,8 @@ const Project = (props) => {
                 <h3>{props.name}</h3>
                 <p>{props.description}</p>
                 <div className="btns-container">
-                    <button href={props.github} className="project-btn" onClick={btnClick}>Github</button>
-                    <button href={props.demo} className="project-btn" onClick={btnClick}>Project</button>
-                    <button href={props.source} className="project-btn" onClick={btnClick}>Source Code</button>
+                    <button href={props.github} className="project-btn" onClick={btnClick}> View Github</button>
+                    <button href={props.demo} className="project-btn" onClick={btnClick}>View Project</button>
                 </div>
             </div>
         </div>

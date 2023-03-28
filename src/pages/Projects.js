0581beb3113c6820c2db7d64190c86631cbc7693
Tyler Cako <tylercako@dynamic-005-005-005-046.5.5.pool.telefonica.de>
2023-03-28 17:40:45 -0600

    restructured file

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,6 +9,7 @@ const Projects = () => {
                 img="https://images.unsplash.com/photo-1653564900892-a0aea40f75ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" 
                 description="A fullstack web/mobile application built with React Native, NodeJS, Express and MongoDB that tracks a user's workout and helps the user improve their lifts as well as display their progress."
                 github="https://github.com/Tyler-Cako/gym-tracker"
+                demo="http://gymtracker.tylercako.com"
             />
             <Project
                 name="Tylercako.com"

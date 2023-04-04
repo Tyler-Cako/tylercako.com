089d08cf7c41ccea56c232078a44744a569923a0
Tyler Cako <tylercako@Tylers-MacBook-Air.local>
2023-04-04 16:04:07 -0600

    fixed home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,15 @@ const Home = () => {
     return(
         <div className="home">
             <Navibar className='nav-fixed'/>
-            <div className="container name">
-                <h1>Tyler Cako</h1>
-                <h3>Fullstack Web Developer</h3>
+            <div className="name-container">
+                <div className="container name">
+                    <h1>Tyler Cako</h1>
+                    <h3>Fullstack Web Developer</h3>
 
-                <div className="button-container">
-                    <a className="button" target="_blank" href="https://drive.google.com/file/d/1whjSnhAt_NslghLJOtNfSAb8-co0_1TJ/view?usp=sharing">View Resume</a>
-                    <a className="button" target="_blank" href="https://github.com/Tyler-Cako">View Github</a>
+                    <div className="button-container">
+                        <a className="button" target="_blank" href="https://drive.google.com/file/d/1whjSnhAt_NslghLJOtNfSAb8-co0_1TJ/view?usp=sharing">View Resume</a>
+                        <a className="button" target="_blank" href="https://github.com/Tyler-Cako">View Github</a>
+                    </div>
                 </div>
             </div>
         </div>
